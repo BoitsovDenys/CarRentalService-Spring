558a36dfe6055fba0e1e21323cfeb9f27317a9bb
BoitsovDenys <boitsov.denys@gmail.com>
2021-08-02 03:00:54 +0300

    Created the project

---
+++ src/main/java/ua/training/CarRentalServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package ua.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalServiceSpringApplication.class, args);
+    }
+
+}
